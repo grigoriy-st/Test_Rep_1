d3ec70dad44026bd9d235810a034ddb29e351c88
Grigoriy <gregorey060@gmail.com>
2024-12-05 14:49:21 +0600

    Задание 2. Git и случайные окружности

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from PyQt6.QtWidgets import (
     QApplication, QMainWindow, QDialog, QVBoxLayout, QWidget,
 )
 from PyQt6.QtGui import QPainter, QColor
-
+from UI import Ui_Dialog
 
 class Main_Window(QDialog):
     def __init__(self):
@@ -16,22 +16,25 @@ class Main_Window(QDialog):
         self.setup_ui()
 
     def setup_ui(self):
-        uic.loadUi("dialog.ui", self)
+        self.ui = Ui_Dialog()
+        self.ui.setupUi(self)
+
         self.circles = []
-        self.button.clicked.connect(self.add_circle)
+        self.ui.button.clicked.connect(self.add_circle)
 
 
     def add_circle(self):
         diameter = random.randint(20, 100)
         x = random.randint(0, self.width() - diameter)
         y = random.randint(0, self.height() - diameter)
-        self.circles.append((x, y, diameter))
+        color = QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+        self.circles.append((x, y, diameter, color))
         self.update()
 
     def paintEvent(self, event):
         painter = QPainter(self)
-        painter.setBrush(QColor("yellow"))
-        for (x, y, diameter) in self.circles:
+        for (x, y, diameter, color) in self.circles:
+            painter.setBrush(color)
             painter.drawEllipse(x, y, diameter, diameter)
 
 if __name__ == "__main__":
